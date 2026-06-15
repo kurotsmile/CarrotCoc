985da119bc30953aaac3291c562de63a6cc18813
Trần Thiện Thanh <kurotsmile@gmail.com>
2026-06-16 01:19:39 +0700

    Fix nore
    Fix nore

--- config/paypal.php
+++ config/paypal.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'client_id' => getenv('PAYPAL_CLIENT_ID') ?: '',
-    'client_secret' => getenv('PAYPAL_CLIENT_SECRET') ?: '',
-    'mode' => getenv('PAYPAL_MODE') ?: 'sandbox',
-    'currency' => getenv('PAYPAL_CURRENCY') ?: 'USD',
+    'client_id' => 'AVztcE8iW6IXH-Yvg1_WSIxnOrKdfmp0ULNcqAMBr9L7U6JYTMZiNQLnZZiLFKbmxVvida9Jpb-Z4_jV',
+    'client_secret' => 'EH9oNDURGEJEdRXeIB4810esYl72M5ydVM_7O48-wu2ULpkNkmAMyifmQPqAT0Bo3EaoQzcKxR2nC2ze',
+    'mode' => 'sandbox',
+    'currency' => 'USD',
 ];
 
