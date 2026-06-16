26746272383f09cad2f60fc33bfae103290e46f5
Trần Thiện Thanh <kurotsmile@gmail.com>
2026-06-17 00:23:04 +0700

    Fix gnore

--- config/paypal.php
+++ config/paypal.php
@@ -3,7 +3,7 @@
 return [
     'client_id' => 'AVztcE8iW6IXH-Yvg1_WSIxnOrKdfmp0ULNcqAMBr9L7U6JYTMZiNQLnZZiLFKbmxVvida9Jpb-Z4_jV',
     'client_secret' => 'EH9oNDURGEJEdRXeIB4810esYl72M5ydVM_7O48-wu2ULpkNkmAMyifmQPqAT0Bo3EaoQzcKxR2nC2ze',
-    'mode' => 'sandbox',
+    'mode' => 'live',
     'currency' => 'USD',
 ];
 
